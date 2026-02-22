e566af43c535552ca88c4647ddbca2304b619fb6
Sikuath <pum.up09@gmail.com>
2026-02-22 16:11:00 +0100

    Create photos.js with a list of gallery images
    Added a list of gallery photos in photos.js.

---
+++ docs/photos.js
@@ -0,0 +1,12 @@
+// Liste des photos de la galerie
+const photos = [
+  'IC1318A_RGB.jpg',
+  'IC1805_SHO.jpg',
+  'M31.jpg',
+  'NGC6883_RGB.jpg',
+  'NGC6888_SHO.jpg',
+  'SH2-119.jpg',
+  'SH2-135.jpg',
+  'SH2-157_SHO.jpg'
+  // Ajoute ici de nouvelles photos en ajoutant juste le nom du fichier
+];
